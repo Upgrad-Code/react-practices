729956185034ed378a83c4fb65cc0f1903ef8faf
Upgrad-Code <112937791+Upgrad-Code@users.noreply.github.com>
2022-12-08 21:05:46 +0530

    2 files modified

---
+++ src/pages/ProductPage/ProductPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const ProductPage = () => {
+  return (
+    <Container>
+      <Row>
+        <Col md={12}>Running product page.</Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default ProductPage;
