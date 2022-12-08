c2624985b1b68d7184c72b6ff8afd470f0eced20
Upgrad-Code <112937791+Upgrad-Code@users.noreply.github.com>
2022-12-08 21:17:17 +0530

    1 file modified

--- src/pages/ProductPage/ProductPage.js
+++ src/pages/ProductPage/ProductPage.js
@@ -1,7 +1,20 @@
 import React, { useReducer, useEffect } from 'react';
+import { reducer, initialState } from '../../reducers/ProductPageReducer';
+import { API_URL } from '../../helpers/config';
+import { getJSON } from '../../helpers/helpersFn';
 import { Container, Row, Col } from 'react-bootstrap';
 
 const ProductPage = () => {
+  const [state, setState] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const data = await getJSON(`${API_URL}/all`);
+      console.log(data);
+    };
+    getProducts();
+  });
+
   return (
     <Container>
       <Row>
