272b5ac844b17c6387d39a9de602e8c6b575c6af
Upgrad-Code <112937791+Upgrad-Code@users.noreply.github.com>
2022-12-08 21:55:50 +0530

    1 file modified

--- src/reducers/ProductPageReducer.js
+++ src/reducers/ProductPageReducer.js
@@ -10,9 +10,9 @@ export const reducer = (state, action) => {
   switch (action.type) {
     case ACTIONS.FETCH_START:
       return { ...state, isLoading: true };
-    case ACTIONS.FETCH_START:
+    case ACTIONS.FETCH_SUCCESS:
       return { ...state, products: action.payload, isLoading: false };
-    case ACTIONS.FETCH_START:
+    case ACTIONS.FETCH_ERROR:
       return { ...state, isError: action.payload, isLoading: false };
     case 'default':
       state;
